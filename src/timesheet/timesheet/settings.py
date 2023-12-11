dc12ba4a80807773aab50427c14e16cb37dbc441
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-11 11:47:33 +0530

    Added login user

--- src/timesheet/timesheet/settings.py
+++ src/timesheet/timesheet/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
-    'timesheet_app'
+    'timesheet_app',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
--- src/timesheet/timesheet_app/views/auth_view.py
+++ src/timesheet/timesheet_app/views/auth_view.py
@@ -2,15 +2,42 @@ from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..serializers.user_serializer import UserSerializer
-
+from rest_framework_simplejwt.tokens import RefreshToken
 
 @api_view(['POST'])
 def register_user(request):
     serializer = UserSerializer(data = request.data)
+    
     if serializer.is_valid(raise_exception=True):
         user = serializer.save()
         user.set_password(serializer.validated_data['password'])
         user.save()
         return Response(serializer.data , status=201)
+    
+    return Response(serializer.error_messages , status=400)
+    
+    
+@api_view(['POST'])
+def login_user(request):
+    serializer = UserSerializer(data = request.data)
+    
+    if serializer.is_valid(raise_exception=True):
+        email = serializer.data['email']
+        password = serializer.data['password']
+        
+        user = authenticate(email , password)
+        
+        if user is None:
+            return Response({"message":{"error": "Invalid Credentials"}} , status=400)
+    
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }
+        )
+    
     return Response(serializer.error_messages , status=400)
     
