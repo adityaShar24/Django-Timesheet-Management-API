9642fe4e760c63d4970f528ebae27d794a252042
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-11 15:48:01 +0530

    Added update project

--- src/timesheet/timesheet_app/views/project_view.py
+++ src/timesheet/timesheet_app/views/project_view.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..serializers.project_serializer import ProjectSerializer
+from ..models.project_model import Project
 
 @api_view(['POST'])
 def create_project(request):
@@ -10,3 +11,12 @@ def create_project(request):
         return Response(serializer.data , status=201)
     return Response(serializer.error_messages , status= 400)
 
+@api_view(["PUT"])
+def update_project(request , pk):
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(instance=project , data=request.data)
+    
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data , status=201)
+    
