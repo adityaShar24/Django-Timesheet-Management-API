33281cab0b997fac6276859414dd2698a34542bd
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-11 15:22:08 +0530

    Added project view

---
+++ src/timesheet/timesheet_app/views/project_view.py
@@ -0,0 +1,12 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from ..serializers.project_serializer import ProjectSerializer
+
+@api_view(['POST'])
+def creat_project(request):
+    serializer = ProjectSerializer(data= request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data , status=201)
+    return Response(serializer.error_messages , status= 400)
+
