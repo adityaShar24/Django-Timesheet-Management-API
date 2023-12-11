f3ae2c753c3a84dd7de1cf0e789e0bf184b878e5
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-11 15:50:22 +0530

    Added update project url

--- src/timesheet/timesheet_app/urls.py
+++ src/timesheet/timesheet_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views.auth_view import register_user , login_user
-from .views.project_view import create_project
+from .views.project_view import create_project , update_project
 
 urlpatterns = [
     path('auth/register/' , register_user , name='register_user'),
     path('auth/login/' , login_user , name='login_user' ),
-    path('project/create/' , create_project , name = 'create_project')
+    path('project/create/' , create_project , name = 'create_project'),
+    path('project/update/' , update_project , name = 'update_project')
 ] 
