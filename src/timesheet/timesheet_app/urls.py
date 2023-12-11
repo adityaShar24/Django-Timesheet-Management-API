a457f8d12cfe93f001187d5d364b7041fbd5d29e
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-11 11:48:51 +0530

    Added login user url

--- src/timesheet/timesheet_app/urls.py
+++ src/timesheet/timesheet_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views.auth_view import register_user
+from .views.auth_view import register_user , login_user
 
 urlpatterns = [
-    path('auth/register/' , register_user , name='register_user')
+    path('auth/register/' , register_user , name='register_user'),
+    path('auth/login/' , login_user , name='login_user' ),
 ] 
