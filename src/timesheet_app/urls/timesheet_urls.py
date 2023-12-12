77ffb1b2c92d63635d5a589ef7171aacb49889af
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-12 16:54:52 +0530

    Added update timesheet endpoint

--- src/timesheet_app/urls/timesheet_urls.py
+++ src/timesheet_app/urls/timesheet_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from ..views.timesheet_view import create_timesheet
+from ..views.timesheet_view import create_timesheet , update_timesheet
 
 
 urlpatterns = [
     path('create' , create_timesheet , name='create_timesheet'),
+    path('update/<str:pk>' , update_timesheet , name='update_timesheet'),
 ]
--- src/timesheet_app/views/timesheet_view.py
+++ src/timesheet_app/views/timesheet_view.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from ..serializers.timesheet_serializer import TimesheetSerializer
+from ..models.timesheet_model import Timesheet
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
@@ -31,4 +32,39 @@ def create_timesheet(request):
         response = Response(serializer.errors , status= HTTP_400_BAD_REQUEST)
         
     return response
+
+
+@api_view(["PUT"])
+def update_timesheet(request , pk):
+    response = None
+    timesheet = Timesheet.objects.get(id= pk)
+
+    if timesheet.user.id != request.user.id:
+        response_data = {
+            "message": "You don't have permission to update this timesheet"
+        }
+        
+        response = Response(response_data , status= HTTP_400_BAD_REQUEST)
+    else:
+        data = {
+            "projects": request.data.get("projects"),
+            "hours_worked": request.data.get("hours_worked"),
+            "week_start_date": request.data.get("week_start_date"),
+            "user": request.user.id
+        }
+        
+        serializer = TimesheetSerializer(instance= timesheet , data= data)
     
+        if serializer.is_valid():
+            serializer.save()
+        
+            response_data = {
+                "message": "Timesheet updated successfully",
+                "data": serializer.data
+            }
+        
+            response = Response(response_data , status= HTTP_201_CREATED)
+        else:
+            response = Response(serializer.errors , status= HTTP_400_BAD_REQUEST)
+        
+    return response
