3d480e78adebdb9c91c1956b809e8dcc6bf815a5
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-12 11:40:38 +0530

    Added logout url

--- src/timesheet/timesheet_app/urls/auth_urls.py
+++ src/timesheet/timesheet_app/urls/auth_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from ..views.auth_view import register_user , login_user
+from ..views.auth_view import register_user , login_user , logout_user
 
 
 urlpatterns = [
     path('register/' , register_user , name='register_user'),
     path('login/' , login_user , name='login_user' ),
+    path('logout/' , logout_user , name='logout_user' ),
 ]
