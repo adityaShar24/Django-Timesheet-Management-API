08f2f421fd9065babc5232ff799b3b097e83bf48
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-12 15:50:00 +0530

    Added create timesheet url

--- src/timesheet/urls.py
+++ src/timesheet/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/project/' , include("timesheet_app.urls.project_urls")),
     path('api/auth/' , include("timesheet_app.urls.auth_urls")),
+    path('api/timesheet/' , include("timesheet_app.urls.timesheet_urls")),
 ]
---
+++ src/timesheet_app/urls/timesheet_urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from ..views.timesheet_view import create_timesheet
+
+
+urlpatterns = [
+    path('/create' , create_timesheet , name='create_timesheet'),
+]
