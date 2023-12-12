4eaa35e8a08ed5b089ff5e9d464e518dabb54862
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-12 11:39:08 +0530

    Added logout user

--- src/timesheet/timesheet_app/utils/constants.py
+++ src/timesheet/timesheet_app/utils/constants.py
@@ -1,5 +1,6 @@
 USER_REGISTERED_MESSAGE = 'user has been registered successfully!'
 USER_LOGGEDIN_MESSAGE = 'user {username} has been loggedin successfully!'
+USER_LOGGEDOUT_MESSAGE = 'user has been loggedout successfully!'
 
 INVAID_CREDENTIALS_MESSAGE = "Invalid username or password"
 
--- src/timesheet/timesheet_app/views/auth_view.py
+++ src/timesheet/timesheet_app/views/auth_view.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate , logout
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..serializers.user_serializer import UserSerializer
 from ..serializers.login_serializer import LoginSerializer
-from rest_framework.status import  HTTP_400_BAD_REQUEST , HTTP_201_CREATED
+from rest_framework.status import  HTTP_400_BAD_REQUEST , HTTP_201_CREATED , HTTP_200_OK , HTTP_401_UNAUTHORIZED
 from rest_framework_simplejwt.tokens import RefreshToken , AccessToken
-from ..utils.constants import USER_REGISTERED_MESSAGE , USER_LOGGEDIN_MESSAGE , INVAID_CREDENTIALS_MESSAGE
+from ..utils.constants import USER_REGISTERED_MESSAGE , USER_LOGGEDIN_MESSAGE , INVAID_CREDENTIALS_MESSAGE , USER_LOGGEDOUT_MESSAGE
 
 @api_view(['POST'])
 def register_user(request):
@@ -52,3 +52,17 @@ def login_user(request):
     
     return Response( serializer.errors , status= HTTP_400_BAD_REQUEST)
     
+
+@api_view(['POST'])
+def logout_user(request):
+    refresh_token = request.data.get('refresh_token')
+
+    if not refresh_token:
+        return Response({"error": "Refresh token is required."}, status= HTTP_401_UNAUTHORIZED)
+
+    try:
+        RefreshToken(refresh_token).blacklist()
+        logout(request)
+        return Response({"message": USER_LOGGEDOUT_MESSAGE}, status= HTTP_200_OK)
+    except Exception as e:
+        return Response({"error": str(e)}, status= HTTP_401_UNAUTHORIZED)
